25d00b9a67988a7fbe24754bce0c102d4d495155
Ahmed Musawir <moose@Ahmeds-Mac.local>
2023-02-01 18:57:12 +0800

    1Feb2023 - footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt={'80px'} bgcolor='#fff3f5'>
+      <Stack gap={'40px'} alignItems='center' px={'40px'} pt='24px'>
+        <img src={Logo} alt='logo' width={'200px'} height='40px' />
+        <Typography variant='h5' pb={'40px'} mt='20px'>
+          Built by Cyberize Group Inc.
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
